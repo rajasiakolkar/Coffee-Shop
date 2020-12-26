8239b548016f6af6ffcfd4f923ab9ff987ccfd12
Rajasi Akolkar <akolkar.r@northeastern.edu>
2020-12-26 14:49:18 -0500

    Adding Data Annotations

--- EspressoAPI/Models/Reservation.cs
+++ EspressoAPI/Models/Reservation.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace EspressoAPI.Models
 {
     public class Reservation
@@ -9,16 +11,25 @@ namespace EspressoAPI.Models
 
         public int ID { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email in not valid")]
         public string Email { get; set; }
 
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid Phone Number")]
         public string Phone { get; set; }
 
+        [Required]
         public int totalPeople { get; set; }
 
+        [Required]
         public DateTime Date { get; set; }
 
+        [Required]
+        [DataType(DataType.DateTime)]
         public string Time { get; set; }
 
     }
